34132bdc073f31367d0a1494094c489df43e2216
Ib Kara <ib00kara00@gmail.com>
2026-04-27 22:04:30 +0000

    Ajout utilisation dans Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        ContactsManager manager = new ContactsManager();
+
+        Contact c1 = new Contact();
+        c1.name = "Kone";
+        c1.phoneNumber = "0102030405";
+
+        manager.addContact(c1);
     }
 }
