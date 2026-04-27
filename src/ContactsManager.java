abd26baba0e4bd368fff5c4382e248d050c5eee1
Ib Kara <ib00kara00@gmail.com>
2026-04-27 21:54:41 +0000

    Ajout de ContactsManager avec constructeur

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,1 +1,9 @@
+public class ContactsManager {
+    Contact[] myFriends;
+    int friendsCount;
 
+    ContactsManager() {
+        this.friendsCount = 0;
+        this.myFriends = new Contact[500];
+    }
+}
