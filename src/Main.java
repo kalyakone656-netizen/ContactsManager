7271d0c00666132a5a641e43ac1d1e2a9de49fe4
Ib Kara <ib00kara00@gmail.com>
2026-04-27 22:10:01 +0000

    Ajout de plusieurs contacts

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,28 @@ public class Main {
         c1.phoneNumber = "0102030405";
 
         manager.addContact(c1);
+
+        Contact c2 = new Contact();
+        c2.name = "Kadi";
+        c2.phoneNumber = "0506124872";
+        manager.addContact(c2);
+
+        // Contact 3
+        Contact c3 = new Contact();
+        c3.name = "Jean";
+        c3.phoneNumber = "0504080906";
+        manager.addContact(c3);
+
+        // Contact 4
+        Contact c4 = new Contact();
+        c4.name = "christ";
+        c4.phoneNumber = "0604081395";
+        manager.addContact(c4);
+
+        // Contact 5
+        Contact c5 = new Contact();
+        c5.name = "yohann";
+        c5.phoneNumber = "0505487925";
+        manager.addContact(c5);
     }
 }
